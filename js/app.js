8ecca63a774479ef442e683c4764dff517022a25
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-04-23 20:57:27 +0200

    Add basic drawing functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 const panelContainer = document.querySelector('.panel-container');
+let mouseDown = false;
 
-for (let i = 0; i < 16*16; i++) {
-  const panel = document.createElement('div');
-  panel.classList.add('panel');
-  panelContainer.appendChild(panel);
+function generatePanelGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const panel = document.createElement('div');
+    panel.classList.add('panel');
+    panelContainer.appendChild(panel);
+  }
+
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach((panel) =>
+    panel.addEventListener('mousedown', () => (mouseDown = true))
+  );
+
+  panels.forEach((panel) =>
+    panel.addEventListener('mouseup', () => (mouseDown = false))
+  );
+
+  panels.forEach((panel) =>
+    panel.addEventListener('mousemove', function () {
+      if (mouseDown) drawColor(this, 'white');
+    })
+  );
 }
+
+function drawColor(panel, color) {
+  panel.setAttribute('style', `background-color: ${color}`);
+}
+
+generatePanelGrid(16);
