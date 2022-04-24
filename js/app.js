43d8a93b4f98e5d53ccc96ea030ed4641e640f48
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-04-24 13:05:06 +0200

    Add draw and erase buttons functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const panelContainer = document.querySelector('.panel-container');
+const drawBtn = document.querySelector('#draw');
+const eraseBtn = document.querySelector('#erase');
 let mouseDown = false;
+const defaultColor = window.getComputedStyle(panelContainer, null).getPropertyValue('background-color');
 
 function generatePanelGrid(size) {
   for (let i = 0; i < size * size; i++) {
@@ -13,12 +16,10 @@ function generatePanelGrid(size) {
   const panels = document.querySelectorAll('.panel');
 
   window.addEventListener('mouseup', () => (mouseDown = false));
+  activateDrawMode();
   panels.forEach((panel) => {
     // Implement drawing capability
     panel.addEventListener('mousedown', () => (mouseDown = true));
-    panel.addEventListener('mousemove', function () {
-      if (mouseDown) drawColor(this, getColor());
-    });
     // Prevent dragging of panels
     panel.addEventListener('dragstart', (e) => e.preventDefault());
   });
@@ -33,4 +34,24 @@ function getColor() {
   return colorPicker.value;
 }
 
+function activateDrawMode() {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach((panel) => {
+    panel.addEventListener('mousemove', function () {
+      if (mouseDown) drawColor(this, getColor());
+    });
+  });
+}
+
+function activateEraseMode() {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach((panel) => {
+    panel.addEventListener('mousemove', function () {
+      if (mouseDown) drawColor(this, defaultColor);
+    });
+  });
+}
+
 generatePanelGrid(16);
+drawBtn.addEventListener('click', activateDrawMode);
+eraseBtn.addEventListener('click', activateEraseMode);
